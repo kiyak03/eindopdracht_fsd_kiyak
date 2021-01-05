82a00405f080bbca916d08870924886b76cc5eb3
fatih <f.kiyak@outlook.com>
2021-01-05 22:51:31 +0100

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+// import PageHeader from "../components/PageHeader";
+import {Link} from "react-router-dom";
+// import Logo from "../assets/logo_hexagon_white.png"
 
 function Home(){
     return(
+        <div>
+            {/*<PageHeader icon={Logo} title="Test title"/>*/}
         <h1>Welkom  dit is de Home pagina!</h1>
+            <Link to={"/upload"}>
+
+                <button type='button'>Drop demo NOW!</button>
+            </Link>
+
+
+        </div>
+
+
     );
 }
 
--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,17 +1,31 @@
 import React from 'react'
-// import './App.css';
 import {NavLink} from "react-router-dom";
+import PageHeader from "./PageHeader";
+import Logo from "../assets/logo_hexagon_white.png";
 
 function TopMenu() {
     return (
         <>
             <nav className="topmenu">
+                <PageHeader icon={Logo} title="Hexagon"/>
                 <div className="topmenu-container">
-                    <NavLink to="/" exact activeClassName="active-link">Home</NavLink>
-                    <NavLink to="/upload"  activeClassName="active-link">Upload</NavLink>
-                    <NavLink to="/login"  activeClassName="active-link">Login</NavLink>
-                    <NavLink to="/register"  activeClassName="active-link">Register</NavLink>
-                    <NavLink to="/contact"  activeClassName="active-link">Contact</NavLink>
+                    <ul>
+                        <il>
+                            <NavLink to="/" exact activeClassName="active-link">Home</NavLink>
+                        </il>
+                        <il>
+                            <NavLink to="/upload"  activeClassName="active-link">Upload</NavLink>
+                        </il>
+                        <il>
+                            <NavLink to="/login"  activeClassName="active-link">Login</NavLink>
+                        </il>
+                        <il>
+                            <NavLink to="/register"  activeClassName="active-link">Register</NavLink>
+                        </il>
+                        <il>
+                            <NavLink to="/contact"  activeClassName="active-link">Contact</NavLink>
+                        </il>
+                    </ul>
                 </div>
 
 
