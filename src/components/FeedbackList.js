61589d36725381b88b67b3722da7a26f3d7a3bf3
fatih <f.kiyak@outlook.com>
2024-08-18 00:23:26 +0300

    delete button for feedback added

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -23,6 +23,23 @@ function FeedbackList({ demoId }) {
 		fetchFeedback();
 	}, [demoId]);
 
+	const handleDelete = async (id) => {
+		try {
+			const token = localStorage.getItem('token');
+			await axios.delete(`http://localhost:8080/feedback/${id}`, {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			// Remove the deleted feedback from the list
+			setFeedbackList(feedbackList.filter((feedback) => feedback.id !== id));
+		} catch (error) {
+			console.error('Error deleting feedback:', error);
+		}
+	};
+
+
 	return (
 		<div>
 			<h3>Feedback:</h3>
@@ -33,6 +50,7 @@ function FeedbackList({ demoId }) {
 					<li key={feedback.id}>
 						<strong>Feedback:</strong> {feedback.feedback}<br/>
 						<strong>Comment:</strong> {feedback.comment}
+						<button onClick={() => handleDelete(feedback.id)}>Delete</button>
 					</li>
 				))}
 			</ul>
